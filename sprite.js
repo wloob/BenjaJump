bdbf1655394d870adb49820411ec9d51db09e8d3
Buggiam <williambugge@hotmail.com>
2017-10-17 20:22:39 +0200

    no img check

--- sprite.js
+++ sprite.js
@@ -6,8 +6,6 @@ function addLoadedImage(name) {
     }
 
     var img = loadImage((useHost ? host : "") + name);
-    console.log("Loading img' " + name + "' from host=" + useHost + ", success:");
-    console.log(img !== null);
 
     if (img !== null)
         loadedImages.push(new LoadedImage(name, img));
